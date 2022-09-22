691d8adca41d6879585418288eb316239d563059
George Reznik <nereznik@gmail.com>
2022-09-22 09:00:12 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 import './index.html'
 import './index.css'
 import './assets/board/board.png'
-import {start, started} from './js/app';
+import { start, started } from './js/app'
 
-document.addEventListener('deviceready', onDeviceReady);
+document.addEventListener('deviceready', onDeviceReady)
 
 async function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-    
-    await start();
-    
-    document.getElementsByClassName('app')[0].remove();
+  // Cordova is now initialized. Have fun!
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
+  document.getElementById('deviceready').classList.add('ready')
+
+  await start()
+
+  document.getElementsByClassName('app')[0].remove()
 }
